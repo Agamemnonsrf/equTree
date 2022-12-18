d9e4e9f496cfcdf8ab54380c0c1ad6e130dcc4f9
Ooey <agiospantas@gmail.com>
2022-12-18 22:57:30 +0200

    singular card functional

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Canvas } from './Components/Canvas';
 import './App.css';
+import { Context } from './context/context';
 
 function App() {
+  const [equations, setEquations] = React.useState([]);
+  const [equation, setEquation] = React.useState('');
+  const [result, setResult] = React.useState(0);
   return (
-    <div className="App">
-      <body>
-        <section className='main'>
-          <Canvas />
-        </section>
-      </body>
-    </div>
+    <Context.Provider value={{ equations, setEquations, result, setResult, equation, setEquation }}>
+      <header className='Header'><h2>{result}</h2></header>
+      <Canvas />
+    </Context.Provider>
   );
 }
 
--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../App.css'
-import {EquCard} from './EquCard';
+import { EquCard } from './EquCard';
+import Draggable from 'react-draggable';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 
 export const Canvas = () => {
+    const { height, width } = useWindowDimensions();
     return (
-        <div className='canvas'>
-            <EquCard/>
-        </div>
+        <div className='canvas' >
+            <EquCard />
+        </div >
     );
 }
