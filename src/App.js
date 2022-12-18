c35aad3ab2a5fd7e90697ee8e63c3e16c902c8a1
Ooey <agiospantas@gmail.com>
2022-12-18 12:00:40 +0200

    init

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            React Redux
+            React Reduxs
           </a>
         </span>
       </header>
